0f533d5cc392b7c01eca90408df62f08e8a4ccef
nemo <git@ningu.net>
2023-12-19 17:47:46 +0100

    Use proper sound device

--- vmse_start.sh
+++ vmse_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 tmux \
     new-session  'cd code/vmse2000 && poetry run python vmse2000.py' \; \
-    split-window 'cd code/v3 && ./start.sh' \;
+    split-window 'cd code/v3 && ./start.sh 1' \;
